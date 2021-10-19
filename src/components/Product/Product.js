c667dbc729afd289f2efbbbbf7fecc7b1a80c465
Tzviya <57465096+tlaniado@users.noreply.github.com>
2021-10-19 18:19:32 +0300

    cart product B

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,73 +1,11 @@
 import "./Product.css";
-import CartProductsContexts from "../../CartProductsContext";
 
-import React, { useContext } from "react";
+import React from "react";
 import AddToCart from "../AddToCart/AddToCart";
 //import AddToCart from "../AddToCart/AddToCart";
 //let quantity = 1;
 
 function Product({ imgSrc, name, price, id }) {
-  const [cartProduct, setCartProduct] = useContext(CartProductsContexts);
-
-  function incrementCount() {
-    let exist = false;
-    cartProduct.map((item) => (item.id === id ? (exist = true) : {}));
-    // console.log(cartProduct.some((item) => item.id !== id));
-    if (!exist) {
-      //console.log("not exited");
-      setCartProduct([
-        {
-          image: imgSrc,
-          name: name,
-          id: id,
-          price: price,
-          quantity: 1,
-        },
-        ...cartProduct,
-      ]);
-    } else {
-      console.log(cartProduct);
-
-      setCartProduct(
-        cartProduct.map((item) =>
-          item.id === id
-            ? {
-                ...item,
-                price: item.price + price,
-                quantity: item.quantity + 1,
-              }
-            : item
-        )
-      );
-    }
-  }
-  function decrementCount() {
-    //cartProduct.some((item) => item.id === id && item.quantity === 1)
-    let exist = false;
-    cartProduct.map((item) =>
-      item.id === id && item.quantity === 1 ? (exist = true) : {}
-    );
-    if (exist) {
-      setCartProduct(cartProduct.filter((item) => item.id !== id));
-    } else {
-      if (cartProduct.some((item) => item.id === id)) {
-        setCartProduct(
-          cartProduct.map((item) =>
-            item.id === id
-              ? {
-                  ...item,
-                  price: item.price - price,
-                  quantity: item.quantity - 1,
-                }
-              : item
-          )
-        );
-      } else {
-        return;
-      }
-    }
-  }
-
   return (
     <div className="product-card">
       <div className="product-image">
